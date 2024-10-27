8c0c4cb4a5a770ad1616c4a01db30e9462210cbd
Shinsuke Sugaya <shinsuke@apache.org>
2024-10-27 13:45:36 +0900

    add test case

--- src/test/java/org/codelibs/fess/ds/git/GitDataStoreTest.java
+++ src/test/java/org/codelibs/fess/ds/git/GitDataStoreTest.java
@@ -15,12 +15,19 @@
  */
 package org.codelibs.fess.ds.git;
 
-import org.codelibs.fess.ds.git.GitDataStore;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.codelibs.fess.crawler.filter.UrlFilter;
+import org.codelibs.fess.ds.callback.IndexUpdateCallback;
+import org.codelibs.fess.entity.DataStoreParams;
+import org.codelibs.fess.es.config.exentity.DataConfig;
 import org.codelibs.fess.util.ComponentUtil;
 import org.dbflute.utflute.lastaflute.LastaFluteTestCase;
+import org.eclipse.jgit.diff.DiffEntry;
 
 public class GitDataStoreTest extends LastaFluteTestCase {
-    public GitDataStore dataStore;
 
     @Override
     protected String prepareConfigFile() {
@@ -35,7 +42,6 @@ public class GitDataStoreTest extends LastaFluteTestCase {
     @Override
     public void setUp() throws Exception {
         super.setUp();
-        dataStore = new GitDataStore();
     }
 
     @Override
@@ -44,8 +50,63 @@ public class GitDataStoreTest extends LastaFluteTestCase {
         super.tearDown();
     }
 
-    public void test_xxx() {
-        // TODO
-        assertTrue(true);
+    public void test_storeData() {
+        DataStoreParams params = new DataStoreParams();
+        params.put("uri", "https://github.com/codelibs/fess-ds-git.git");
+        params.put("base_url", "https://github.com/codelibs/fess-ds-git/blob/master/");
+        params.put("extractors",
+                "text/.*:textExtractor,application/xml:textExtractor,application/javascript:textExtractor,application/json:textExtractor,application/x-sh:textExtractor,application/x-bat:textExtractor,audio/.*:filenameExtractor,chemical/.*:filenameExtractor,image/.*:filenameExtractor,model/.*:filenameExtractor,video/.*:filenameExtractor,");
+        final List<String> urlList = new ArrayList<>();
+        GitDataStore dataStore = new GitDataStore() {
+            @Override
+            protected UrlFilter getUrlFilter(final DataStoreParams paramMap) {
+                return new MockUrlFilter();
+            }
+
+            @Override
+            protected void processFile(final DataConfig dataConfig, final IndexUpdateCallback callback, final DataStoreParams paramMap,
+                    final Map<String, String> scriptMap, final Map<String, Object> defaultDataMap, final Map<String, Object> configMap) {
+                final DiffEntry diffEntry = (DiffEntry) configMap.get(DIFF_ENTRY);
+                final String path = diffEntry.getNewPath();
+                urlList.add(path);
+            }
+        };
+        dataStore.storeData(null, null, params, null, null);
+
+        assertTrue(urlList.stream().anyMatch(s -> s.endsWith("pom.xml")));
+        assertTrue(urlList.size() > 0);
+    }
+
+    public static class MockUrlFilter implements UrlFilter {
+
+        @Override
+        public void init(String sessionId) {
+            // no-op
+        }
+
+        @Override
+        public boolean match(String url) {
+            return true;
+        }
+
+        @Override
+        public void addInclude(String urlPattern) {
+            // no-op
+        }
+
+        @Override
+        public void addExclude(String urlPattern) {
+            // no-op
+        }
+
+        @Override
+        public void processUrl(String url) {
+            // no-op
+        }
+
+        @Override
+        public void clear() {
+            // no-op
+        }
     }
 }
